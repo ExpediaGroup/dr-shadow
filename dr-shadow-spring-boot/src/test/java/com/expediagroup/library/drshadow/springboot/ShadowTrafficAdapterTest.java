cbc47f73af542eb45687a66247b68bcbf71f925c
Tejas Kumthekar <tejas.kumthekar26@gmail.com>
2019-08-24 12:39:33 -0500

    Add WebClient

--- dr-shadow-spring-boot/src/test/java/com/expediagroup/library/drshadow/springboot/ShadowTrafficAdapterTest.java
+++ dr-shadow-spring-boot/src/test/java/com/expediagroup/library/drshadow/springboot/ShadowTrafficAdapterTest.java
@@ -15,16 +15,17 @@
  */
 package com.expediagroup.library.drshadow.springboot;
 
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
-import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
-import org.springframework.web.client.AsyncRestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import javax.servlet.http.HttpServletRequest;
@@ -36,10 +37,12 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Random;
 import java.util.Vector;
+import java.util.function.Consumer;
 
 import static com.expediagroup.library.drshadow.springboot.ShadowTrafficAdapter.IS_SHADOW_TRAFFIC_KEY;
 import static com.expediagroup.library.drshadow.springboot.ShadowTrafficAdapter.IS_SHADOW_TRAFFIC_VALUE;
 import static com.expediagroup.library.drshadow.springboot.ShadowTrafficAdapter.SHADOW_TRAFFIC_FROM_KEY;
+import static org.mockito.ArgumentMatchers.isNull;
 import static org.mockito.Mockito.any;
 import static org.mockito.Mockito.eq;
 import static org.mockito.Mockito.times;
@@ -52,7 +55,6 @@ import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
 public class ShadowTrafficAdapterTest {
-
     private ShadowTrafficAdapter shadowTrafficAdapter;
 
     @Mock
@@ -68,17 +70,29 @@ public class ShadowTrafficAdapterTest {
     private ShadowTrafficConfigHelper shadowTrafficConfigHelper;
 
     @Mock
-    private AsyncRestTemplate restTemplate;
+    private WebClient webClient;
 
     @Mock
     private Random random;
 
     private String machineName = "testMachineName";
 
+    private WebClient.RequestBodyUriSpec requestBodyUriSpec = Mockito.mock(WebClient.RequestBodyUriSpec.class);
+    private WebClient.RequestBodySpec requestBodySpec = Mockito.mock(WebClient.RequestBodySpec.class);
+    private WebClient.RequestHeadersSpec requestHeadersSpec = Mockito.mock(WebClient.RequestHeadersSpec.class);
+    private WebClient.ResponseSpec responseSpec = Mockito.mock(WebClient.ResponseSpec.class);
+
     @Before
     public void setUp() {
-        shadowTrafficAdapter = new ShadowTrafficAdapter(shadowTrafficConfigHelper, restTemplate, machineName);
+        shadowTrafficAdapter = new ShadowTrafficAdapter(shadowTrafficConfigHelper, machineName);
         shadowTrafficAdapter.setRandom(random);
+        shadowTrafficAdapter.setWebClient(webClient);
+
+        when(webClient.method(any())).thenReturn(requestBodyUriSpec);
+        when(requestBodyUriSpec.uri(any(URI.class))).thenReturn(requestBodySpec);
+        when(requestBodySpec.headers(any())).thenReturn(requestBodySpec);
+        when(requestBodySpec.syncBody(any())).thenReturn(requestHeadersSpec);
+        when(requestHeadersSpec.retrieve()).thenReturn(responseSpec);
     }
 
     @Test
@@ -88,7 +102,7 @@ public class ShadowTrafficAdapterTest {
 
         shadowTrafficAdapter.invokeShadowTraffic(shadowServletRequest, originalServletRequest);
 
-        verify(restTemplate, times(0)).exchange(any(), any(), any(), eq(String.class));
+        verify(webClient, times(0)).method(any());
     }
 
     @Test
@@ -100,7 +114,7 @@ public class ShadowTrafficAdapterTest {
 
         shadowTrafficAdapter.invokeShadowTraffic(shadowServletRequest, originalServletRequest);
 
-        verify(restTemplate, times(0)).exchange(any(), any(), any(), eq(String.class));
+        verify(webClient, times(0)).method(any());
     }
 
     @Test
@@ -112,7 +126,7 @@ public class ShadowTrafficAdapterTest {
 
         shadowTrafficAdapter.invokeShadowTraffic(null, originalServletRequest);
 
-        verify(restTemplate, times(0)).exchange(any(), any(), any(), eq(String.class));
+        verify(webClient, times(0)).method(any());
     }
 
     @Test
@@ -124,7 +138,7 @@ public class ShadowTrafficAdapterTest {
 
         shadowTrafficAdapter.invokeShadowTraffic(shadowServletRequest, null);
 
-        verify(restTemplate, times(0)).exchange(any(), any(), any(), eq(String.class));
+        verify(webClient, times(0)).method(any());
     }
 
     @Test
@@ -137,7 +151,7 @@ public class ShadowTrafficAdapterTest {
 
         shadowTrafficAdapter.invokeShadowTraffic(shadowServletRequest, originalServletRequest);
 
-        verify(restTemplate, times(0)).exchange(any(), any(), any(), eq(String.class));
+        verify(webClient, times(0)).method(any());
         verify(shadowServletRequest, times(1)).getMethod();
     }
 
@@ -160,14 +174,10 @@ public class ShadowTrafficAdapterTest {
         UriComponentsBuilder uriCompBuilder = UriComponentsBuilder.fromHttpUrl("https://int-maui.karmalab.net/hotels?param=abcd");
         URI expectedShadowUrl = uriCompBuilder.build().toUri();
 
-        HttpHeaders expectedHeaders = new HttpHeaders();
-        expectedHeaders.add(IS_SHADOW_TRAFFIC_KEY, IS_SHADOW_TRAFFIC_VALUE);
-        expectedHeaders.add(SHADOW_TRAFFIC_FROM_KEY, machineName);
-
-        HttpEntity<String> httpEntity = new HttpEntity<>(null,
-                expectedHeaders);
-
-        verify(restTemplate, times(1)).exchange(eq(expectedShadowUrl), eq(HttpMethod.GET), eq(httpEntity), eq(String.class));
+        verify(webClient, times(1)).method(eq(HttpMethod.GET));
+        verify(requestBodyUriSpec, times(1)).uri(eq(expectedShadowUrl));
+        verify(requestBodySpec, times(1)).headers(any(Consumer.class));
+        verify(requestBodySpec, times(1)).syncBody(isNull());
     }
 
     @Test
@@ -189,14 +199,10 @@ public class ShadowTrafficAdapterTest {
         UriComponentsBuilder uriCompBuilder = UriComponentsBuilder.fromHttpUrl("http://int-maui.karmalab.net/hotels?param=abcd");
         URI expectedShadowUrl = uriCompBuilder.build().toUri();
 
-        HttpHeaders expectedHeaders = new HttpHeaders();
-        expectedHeaders.add(IS_SHADOW_TRAFFIC_KEY, IS_SHADOW_TRAFFIC_VALUE);
-        expectedHeaders.add(SHADOW_TRAFFIC_FROM_KEY, machineName);
-
-        HttpEntity<String> httpEntity = new HttpEntity<>(null,
-                expectedHeaders);
-
-        verify(restTemplate, times(1)).exchange(eq(expectedShadowUrl), eq(HttpMethod.GET), eq(httpEntity), eq(String.class));
+        verify(webClient, times(1)).method(eq(HttpMethod.GET));
+        verify(requestBodyUriSpec, times(1)).uri(eq(expectedShadowUrl));
+        verify(requestBodySpec, times(1)).headers(any(Consumer.class));
+        verify(requestBodySpec, times(1)).syncBody(isNull());
     }
 
     @Test
@@ -219,14 +225,10 @@ public class ShadowTrafficAdapterTest {
         UriComponentsBuilder uriCompBuilder = UriComponentsBuilder.fromHttpUrl("http://int-maui.karmalab.net/hotels?param=ab cd");
         URI expectedShadowUrl = uriCompBuilder.build().toUri();
 
-        HttpHeaders expectedHeaders = new HttpHeaders();
-        expectedHeaders.add(IS_SHADOW_TRAFFIC_KEY, IS_SHADOW_TRAFFIC_VALUE);
-        expectedHeaders.add(SHADOW_TRAFFIC_FROM_KEY, machineName);
-
-        HttpEntity<String> httpEntity = new HttpEntity<>(null,
-                expectedHeaders);
-
-        verify(restTemplate, times(1)).exchange(eq(expectedShadowUrl), eq(HttpMethod.GET), eq(httpEntity), eq(String.class));
+        verify(webClient, times(1)).method(eq(HttpMethod.GET));
+        verify(requestBodyUriSpec, times(1)).uri(eq(expectedShadowUrl));
+        verify(requestBodySpec, times(1)).headers(any(Consumer.class));
+        verify(requestBodySpec, times(1)).syncBody(isNull());
     }
 
     @Test
@@ -250,59 +252,29 @@ public class ShadowTrafficAdapterTest {
         UriComponentsBuilder uriCompBuilder = UriComponentsBuilder.fromHttpUrl("http://int-maui.karmalab.net/hotels?param=ab cd");
         URI expectedShadowUrl = uriCompBuilder.build().toUri();
 
-        HttpHeaders expectedHeaders = new HttpHeaders();
-        expectedHeaders.add(IS_SHADOW_TRAFFIC_KEY, IS_SHADOW_TRAFFIC_VALUE);
-        expectedHeaders.add(SHADOW_TRAFFIC_FROM_KEY, machineName);
-
-        HttpEntity<String> httpEntity = new HttpEntity<>("testBody",
-                expectedHeaders);
-
-        verify(restTemplate, times(1)).exchange(eq(expectedShadowUrl), eq(HttpMethod.GET), eq(httpEntity), eq(String.class));
+        verify(webClient, times(1)).method(eq(HttpMethod.GET));
+        verify(requestBodyUriSpec, times(1)).uri(eq(expectedShadowUrl));
+        verify(requestBodySpec, times(1)).headers(any(Consumer.class));
+        verify(requestBodySpec, times(1)).syncBody(eq("testBody"));
     }
 
     @Test
-    public void testInvokeShadowTraffic_verifyWithCustomHeaders() {
-
-        when(shadowTrafficConfigHelper.getConfig()).thenReturn(shadowTrafficConfig);
-        when(shadowTrafficConfig.getPercentage()).thenReturn(50);
-        when(random.nextInt(eq(100))).thenReturn(30);
-        when(shadowServletRequest.getMethod()).thenReturn("GET");
-
+    public void testHeaderCreationWithCustomHeaders() {
         Map<String, String> customHeaders = new HashMap<>();
         customHeaders.put("carlson", "tse");
 
-        when(shadowTrafficConfig.getCustomHeaders()).thenReturn(customHeaders);
-        when(shadowTrafficConfig.getForwardHeaders()).thenReturn(new ArrayList<>());
-
-        // Test with 1 http and 1 https
-        when(shadowTrafficConfig.getHosts()).thenReturn(Arrays.asList("http://int-maui.karmalab.net"));
-        // make sure that the param here is url encoded
-        when(shadowServletRequest.getRequestURI()).thenReturn("/hotels?param=ab%20cd");
-
-        shadowTrafficAdapter.invokeShadowTraffic(shadowServletRequest, originalServletRequest);
-
-        UriComponentsBuilder uriCompBuilder = UriComponentsBuilder.fromHttpUrl("http://int-maui.karmalab.net/hotels?param=ab cd");
-        URI expectedShadowUrl = uriCompBuilder.build().toUri();
+        HttpHeaders httpHeaders = shadowTrafficAdapter.createHeaders(shadowServletRequest, customHeaders, new ArrayList<>());
 
         HttpHeaders expectedHeaders = new HttpHeaders();
         expectedHeaders.add(IS_SHADOW_TRAFFIC_KEY, IS_SHADOW_TRAFFIC_VALUE);
         expectedHeaders.add(SHADOW_TRAFFIC_FROM_KEY, machineName);
         expectedHeaders.add("shadow-traffic-carlson", "tse");
 
-        HttpEntity<String> httpEntity = new HttpEntity<>(null,
-                expectedHeaders);
-
-        verify(restTemplate, times(1)).exchange(eq(expectedShadowUrl), eq(HttpMethod.GET), eq(httpEntity), eq(String.class));
+        Assert.assertEquals(expectedHeaders, httpHeaders);
     }
 
     @Test
-    public void testInvokeShadowTraffic_verifyApplicationJsonUTF8Set() {
-
-        when(shadowTrafficConfigHelper.getConfig()).thenReturn(shadowTrafficConfig);
-        when(shadowTrafficConfig.getPercentage()).thenReturn(50);
-        when(random.nextInt(eq(100))).thenReturn(30);
-        when(shadowServletRequest.getMethod()).thenReturn("GET");
-
+    public void testHeaderCreationWithApplicationJsonUTF8Set() {
         Vector headerNames = new Vector();
         headerNames.add("Content-Type");
 
@@ -311,28 +283,13 @@ public class ShadowTrafficAdapterTest {
         when(shadowServletRequest.getHeaderNames()).thenReturn(headers);
         when(shadowServletRequest.getHeader(eq("Content-Type"))).thenReturn(MediaType.APPLICATION_JSON_VALUE);
 
-        when(shadowTrafficConfig.getCustomHeaders()).thenReturn(new HashMap<>());
-        when(shadowTrafficConfig.getForwardHeaders()).thenReturn(Arrays.asList("Content-Type"));
-
-        // Test with 1 http and 1 https
-        when(shadowTrafficConfig.getHosts()).thenReturn(Arrays.asList("http://int-maui.karmalab.net"));
-        // make sure that the param here is url encoded
-        when(shadowServletRequest.getRequestURI()).thenReturn("/hotels?param=ab%20cd");
-
-        shadowTrafficAdapter.invokeShadowTraffic(shadowServletRequest, originalServletRequest);
-
-        UriComponentsBuilder uriCompBuilder = UriComponentsBuilder.fromHttpUrl("http://int-maui.karmalab.net/hotels?param=ab cd");
-        URI expectedShadowUrl = uriCompBuilder.build().toUri();
+        HttpHeaders httpHeaders = shadowTrafficAdapter.createHeaders(shadowServletRequest, new HashMap<>(), Arrays.asList("Content-Type"));
 
         HttpHeaders expectedHeaders = new HttpHeaders();
         expectedHeaders.add(IS_SHADOW_TRAFFIC_KEY, IS_SHADOW_TRAFFIC_VALUE);
         expectedHeaders.add(SHADOW_TRAFFIC_FROM_KEY, machineName);
         expectedHeaders.add("Content-Type", MediaType.APPLICATION_JSON_UTF8_VALUE);
 
-        HttpEntity<String> httpEntity = new HttpEntity<>(null,
-                expectedHeaders);
-
-        verify(restTemplate, times(1)).exchange(eq(expectedShadowUrl), eq(HttpMethod.GET), eq(httpEntity), eq(String.class));
+        Assert.assertEquals(expectedHeaders, httpHeaders);
     }
-
 }
